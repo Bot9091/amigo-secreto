05452fe44a654b76a7cac4dccd8340da87597346
David Cortes <luis.d.cortes@outlook.com>
2025-03-06 19:56:33 -0500

    se agregan las demas funcionalidades al codigo

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //Crear una array para almacenar la informacion 
 const amigos = []
+// Se crea una constante para guardar el elemento input
 const inputName = document.getElementById("amigo")
-
+//Se crea una constante para acceder a la lista de amigos
+const list = document.getElementById("listaAmigos")
 
 
 function agregarAmigo() {
-      
-    if (inputName.value===""){
+    const nombreAmigo= inputName.value.trim()  
+    if (nombreAmigo===""){
         alert("Por favor, inserte un nombre")
         return //se usa el return para que una cadena vacia no haga parte del lengh del array
     }else if (amigos.includes(inputName.value)) {
@@ -14,16 +16,15 @@ function agregarAmigo() {
         return
     }
 
-    if ((inputName)=>inputName.value == Number.parseInt(inputName.value)){
+    if (!isNaN(inputName.value.trim())) {
         alert("Estas ingresando un numero, por favor ingresa un nombre")
         return
     }
 
     amigos.push(inputName.value)
-    console.log(amigos)
-
+    
     inputName.value=""
-       
+    actualizarLista()   
 }
 // se agrega la funcionalidad de agregar un amigo al presionar la tecla enter
 inputName.addEventListener("keypress",(event) =>{
@@ -31,3 +32,19 @@ inputName.addEventListener("keypress",(event) =>{
         agregarAmigo()
     }
 })
+
+inputName.addEventListener("blur",function()
+{actualizarLista})
+
+function actualizarLista(){
+    list.innerHTML=""
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li")
+        li.textContent=amigos[i]
+        list.appendChild(li)
+        
+    }
+    alert("La lista ha sido actualizada")
+}
+
+function 
